432b2833a83d25d826dc12a47d6749650a7b6a6a
Pedro Henrique <nukhes@protonmail.com>
2024-04-24 15:51:01 -0300

    modificação no TODO

--- bin/lcompany.dart
+++ bin/lcompany.dart
@@ -4,7 +4,7 @@ import 'package:lcompany/skills.dart';
 /* 
 
 TODO
-- Criar um metodo de ataque, isso deve ser feito dentro da classe "Entities", esse metodo deve receber o ID do oponente e diminuir seu HP caso tudo de certo.
+- Criar um sistema de ataque (conjunto de metodos), isso deve ser feito dentro da classe "Entities", esse sistema deve receber o ID do oponente e diminuir seu HP caso tudo de certo.
 
 */
 
